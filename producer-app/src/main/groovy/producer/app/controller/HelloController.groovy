eb0acfd58be3d7ad378596b6db62f1e972987836
Jean Tessier <jean@jeantessier.com>
2019-02-16 13:11:05 -0800

    Add simple controller that triggers event message

---
+++ producer-app/src/main/groovy/producer/app/controller/HelloController.groovy
@@ -0,0 +1,24 @@
+package producer.app.controller
+
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+
+import javax.inject.*
+
+import producer.app.event.EventProducer
+
+@Controller("/hello")
+class HelloController {
+
+    @Inject
+    EventProducer eventProducer
+
+    @Get(produces = MediaType.TEXT_PLAIN)
+    def index() {
+        eventProducer.send("Hello World")
+
+        return "OK"
+    }
+
+}
