9fdea20395472d9a9a1372304d8eef5d785fdc8e
Jean Tessier <jean@jeantessier.com>
2019-02-16 22:47:53 -0800

    Create RabbitMQ queue and binding at application startup

---
+++ consumer-app/src/main/groovy/consumer/app/rabbitmq/Topology.groovy
@@ -0,0 +1,38 @@
+package consumer.app.rabbitmq
+
+import com.rabbitmq.client.Channel
+import io.micronaut.configuration.rabbitmq.connect.ChannelPool
+import io.micronaut.context.event.BeanCreatedEvent
+import io.micronaut.context.event.BeanCreatedEventListener
+
+import javax.inject.Singleton
+
+@Singleton
+class Topology implements BeanCreatedEventListener<ChannelPool> {
+
+    @Override
+    ChannelPool onCreated(BeanCreatedEvent<ChannelPool> event) {
+        ChannelPool pool = event.bean
+        Channel channel = null
+        try {
+            channel = pool.channel
+        } catch (IOException e) {
+            // The channel couldn't be retrieved
+        }
+
+        if (channel != null) {
+            try {
+                channel.exchangeDeclare("micronaut.rabbitmq.test.events", "topic")
+                channel.queueDeclare("micronaut.rabbitmq.test.events.consumer", false, false, false, [:])
+                channel.queueBind("micronaut.rabbitmq.test.events.consumer", "micronaut.rabbitmq.test.events", "#")
+            } catch (any) {
+                //no-op
+            } finally {
+                pool.returnChannel(channel)
+            }
+        }
+
+        pool
+    }
+
+}
