7ca0223b28caaa7eb1630cbbcc471423fd48e127
Jean Tessier <jean@jeantessier.com>
2024-06-12 17:25:24 -0700

    Switch to non-blocking producer
    The producer was throwing errors about blocking calls.  The HTTP caller
    was getting errors even though the messages were being queued in RabbitMQ.
    
    Fix based on the [`micronaut-rabbitmq` documentation](https://micronaut-projects.github.io/micronaut-rabbitmq/latest/guide/#producerAcknowledge).

--- producer-app/src/main/groovy/producer/app/event/EventProducer.groovy
+++ producer-app/src/main/groovy/producer/app/event/EventProducer.groovy
@@ -3,10 +3,12 @@ package producer.app.event
 import io.micronaut.rabbitmq.annotation.Binding
 import io.micronaut.rabbitmq.annotation.RabbitClient
 
+import java.util.concurrent.CompletableFuture
+
 @RabbitClient("micronaut.rabbitmq.test.events")
 abstract class EventProducer {
 
-    abstract send(@Binding String routingKey, Event event)
+    abstract CompletableFuture<Void> send(@Binding String routingKey, Event event)
 
     def send(Event event) {
         send(event.getClass().name, event)
